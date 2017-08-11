ce6eb843bb92f626676c279b39b4551e949dc918
JunoJunho <wnsgh611@gmail.com>
2017-08-11 22:47:53 +0900

    성경 리소스 싱글톤 객체로 분리 위한 클래스 생성

---
+++ BibleProject/Model/BibleResource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibleProject.Model
+{
+    public sealed class BibleResource
+    {
+        static readonly BibleResource _instance = new BibleResource();
+
+        public static BibleResource Instance
+        {
+            get { return _instance};
+        }
+
+        // Constructor
+        private BibleResource()
+        {
+
+        }
+    }
+}
