2f9abc38444cd097d8b58de567543a52998dfe7b
JunoJunho <wnsgh611@gmail.com>
2017-08-11 23:22:23 +0900

    성경 아이템 리소스 저장을 위한 키 3개 (책, 장, 절 - 내용) 딕셔너리 정의

--- BibleProject/Model/BibleResource.cs
+++ BibleProject/Model/BibleResource.cs
@@ -8,17 +8,32 @@ namespace BibleProject.Model
 {
     public sealed class BibleResource
     {
-        static readonly BibleResource _instance = new BibleResource();
+        // Singleton Object
+        private static volatile BibleResource _instance = null;
+        private static readonly object _synclock = new object();
+
+        // Attribute
+        Dictionary<String, BIbleChapterItem> bible = null; 
 
         public static BibleResource Instance
         {
-            get { return _instance};
+            get {
+                if (_instance != null) return _instance;
+                lock (_synclock)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new BibleResource();
+                    }
+                }
+                return _instance;
+            }
         }
 
         // Constructor
         private BibleResource()
         {
-
+            this.bible = new Dictionary<string, BIbleChapterItem>();
         }
     }
 }
